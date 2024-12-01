a9424d52f6a75179824984cca21cfd74a4e35886
Isaac McAuley <mcauleyisaac@gmail.com>
2024-11-30 22:47:58 -0500

    Can click definition of inheritance

--- lib/ruby_lsp/ruby_lsp_fabrication/definition.rb
+++ lib/ruby_lsp/ruby_lsp_fabrication/definition.rb
@@ -14,6 +14,22 @@ module RubyLsp
       end
 
       def on_symbol_node_enter(node)
+        if @node_context.call_node.message == 'Fabricator' && inherited?(node)
+          entries = @index["#{node.value}Fabricator"]
+
+          return if entries.blank? || entries.empty?
+
+          entries.each do |entry|
+            @response_builder << Interface::LocationLink.new(
+              target_uri: URI::Generic.from_path(path: entry.file_path).to_s,
+              target_range: range_from_location(entry.location),
+              target_selection_range: range_from_location(entry.name_location)
+            )
+          end
+
+          return
+        end
+
         return unless @node_context.call_node.message == 'Fabricate'
 
         fabricator_name = generate_fabricator_name(@node_context.call_node)
@@ -27,6 +43,21 @@ module RubyLsp
             target_selection_range: range_from_location(entry.name_location)
           )
         end
+      end
+
+      def inherited?(node)
+        @node_context.parent.compact_child_nodes.first.compact_child_nodes.each do |n|
+          next unless n.type == :keyword_hash_node
+
+          first_child = n.compact_child_nodes.first
+          next unless first_child.type == :assoc_node
+          next unless first_child.key.value == 'from'
+          next unless first_child.value.value == node.value
+
+          return true
+        end
+
+        false
       end
 
       def generate_fabricator_name(node)
